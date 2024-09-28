244f1ea13bee2bbd5b86fe3751a8456e508a9731
FoxMine92 <lampone.frizz@gmail.com>
2024-09-28 18:45:56 +0200

    sdf

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    const elemento = document.getElementsByClassName('randomdelay');
+}
+
+window.onAnimationEnd = function(elemento){
+}
---
+++ js/login.js
@@ -0,0 +1,61 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCHz94SZ-y1riPeFsh5RgoABVrDWqHNxC0",
+    authDomain: "dino-crisis-57eef.firebaseapp.com",
+    databaseURL: "https://dino-crisis-57eef-default-rtdb.firebaseio.com",
+    projectId: "dino-crisis-57eef",
+    storageBucket: "dino-crisis-57eef.appspot.com",
+    messagingSenderId: "577840789616",
+    appId: "1:577840789616:web:37d6fa0d70e1606a91dcb5",
+    measurementId: "G-SHCZ9NRHH8"
+
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  const analytics = getAnalytics(app);
+  
+  
+window.onloads = function(){
+
+}
+
+window.logins = async function(){
+    if((await getDataForNode(('')))){
+
+    }else{
+
+    }
+}
+
+window.getDataForNode = async function (nodeId) {
+    const dbRef = ref(database, `/${nodeId}`);
+    try {
+        const snapshot = await get(dbRef);
+        if (snapshot.exists()) {
+            const data = snapshot.val();
+            localStorage.setItem(nodeId,JSON.stringify(data));
+            return 1
+        } else {
+            console.log(`No data found for node ${nodeId}`);
+            return null;
+        }
+    } catch (error) {
+        console.error("Error getting data:", error);
+        return null
+    }
+};
+
+window.addElementToNode = async function (nodeId, elementData) {
+    const dbRef = ref(database, `/${nodeId}`); // Ottieni il riferimento alla sotto-cartella
+
+    try {
+        // Usa push() per aggiungere un nuovo elemento senza sovrascrivere
+        const newElementRef = push(dbRef); // Crea un riferimento per un nuovo elemento
+        await set(newElementRef, elementData); // Imposta i dati dell'elemento
+    } catch (error) {
+        console.error("Errore durante l'aggiunta dell'elemento:", error);
+    }
+};
